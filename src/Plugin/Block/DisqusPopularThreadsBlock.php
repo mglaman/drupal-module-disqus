9b137d7a907af5960774f8c2f158f0f4307c0055
Janez Urevc <janez@janezurevc.name>
2016-02-01 00:03:02 +0530

    by slashrsm: Clean block plugins.

--- src/Plugin/Block/DisqusPopularThreadsBlock.php
+++ src/Plugin/Block/DisqusPopularThreadsBlock.php
@@ -7,9 +7,6 @@
 
 namespace Drupal\disqus\Plugin\Block;
 
-use Drupal\core\Block\Annotation\Block;
-use Drupal\Core\Annotation\Translation;
-
 /**
  *
  * @Block(
@@ -19,15 +16,12 @@ use Drupal\Core\Annotation\Translation;
  * )
  */
 class DisqusPopularThreadsBlock extends DisqusBaseBlock {
-  protected $id = 'disqus_popular_threads';
 
   /**
    * {@inheritdoc}
    */
-  public function build() {
-    return array(
-      '#title' => t('Popular Threads'),
-      $this->render('popular_threads_widget')
-    );
+  public function functionId() {
+    return 'popular_threads_widget';
   }
+
 }
